dad1483b7e2ab18ad928f97df887da7aef8e946e
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-02-06 19:02:35 +0200

    Add systemd .service unit file

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,5 +1,5 @@
 rm -r build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local/leeviuotinen-web -G "MinGW Makefiles"
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/leeviuotinen-web -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles"
 cmake --build . --target install
