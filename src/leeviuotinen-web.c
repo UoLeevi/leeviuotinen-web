1d68e6341d82cf4ab17158398f3b3fe07b1fbf7d
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-03-17 01:26:25 +0200

    Adapt to changes in libuo API

--- src/leeviuotinen-web.c
+++ src/leeviuotinen-web.c
@@ -9,9 +9,9 @@
 void http_server_before_send_response(
     uo_cb *cb)
 {
-    uo_http_sess *http_sess = uo_cb_stack_index(cb, 0);
+    uo_http_conn *http_conn = uo_cb_stack_index(cb, 0);
 
-    uo_http_msg_set_header(http_sess->http_response, "server", "libuo http");
+    uo_http_msg_set_header(&http_conn->http_res, "server", "libuo http");
     uo_cb_invoke(cb);
 }
 
